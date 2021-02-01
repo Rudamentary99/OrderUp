288c7062059415b8100365c0ec6cbd0d18806d5f
Nathan McCloskey <49048346+Rudamentary99@users.noreply.github.com>
2021-01-31 18:59:34 -0600

    added wrappers

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Provider as PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
+const Stack = createStackNavigator();
+
+const tempScreen = ({ navigation }) => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+    <StatusBar style="auto" />
+    <Button
+      mode="contained"
+      onPress={() => {
+        navigation.navigate("sec");
+      }}
+    >
+      test button
+    </Button>
+  </View>
+);
+const secScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>This is the other screen</Text>
+    </View>
+  );
+};
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="temp">
+          <Stack.Screen name="temp" component={tempScreen} />
+          <Stack.Screen name="sec" component={secScreen}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
