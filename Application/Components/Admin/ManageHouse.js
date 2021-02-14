dd6b1aee9ead66a6be6a0f143c1d1a2db4f1ed07
Nathan McCloskey <49048346+Rudamentary99@users.noreply.github.com>
2021-02-14 14:15:31 -0600

    added bottomTabNavigator

--- Application/Components/Admin/ManageHouse.js
+++ Application/Components/Admin/ManageHouse.js
@@ -1,28 +1,49 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import RoundButton from "../helpers/RoundButton";
-const ManageHouse = (props) => {
-  const { navigation } = props;
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { Text, useTheme } from "react-native-paper";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+const Tab = createMaterialBottomTabNavigator();
 
+const ManageTable = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>ManageHouse</Text>
-      <RoundButton
-        style={{
-          position: "absolute",
-          bottom: 0,
-          right: 0,
-          margin: 50,
-          height: 100,
-          width: 100,
-        }}
-        onPress={() => {
-          navigation.navigate("admin-overview");
-        }}
-      >
-        +
-      </RoundButton>
+      <Text>Manage Table</Text>
     </View>
+  );
+};
+
+const ManageHouse = (props) => {
+  return (
+    <Tab.Navigator initialRouteName="Manage Table" shifting>
+      <Tab.Screen
+        name="Manage Servers"
+        component={ManageTable}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="account-multiple"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      ></Tab.Screen>
+      <Tab.Screen
+        name="Manage Table"
+        component={ManageTable}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="table-furniture"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      ></Tab.Screen>
+    </Tab.Navigator>
   );
 };
 export default ManageHouse;
