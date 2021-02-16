f4b2368a1c99487f3cddfcc8671663bb38290853
Nathan McCloskey <49048346+Rudamentary99@users.noreply.github.com>
2021-02-15 20:21:27 -0600

    got api call working

--- Application/Components/Admin/ManageHouse/ManageFloor/FloorController.js
+++ Application/Components/Admin/ManageHouse/ManageFloor/FloorController.js
@@ -1,6 +1,17 @@
-const createNewFloor = (floorName) => {
-  console.log("floorName", floorName);
-};
+const axios = require("axios");
+axios.defaults.baseURL = "http://172.25.19.198:3000";
+async function createNewFloor(floorName) {
+  try {
+    const resp = await axios.post("/api/testBody", {
+      name: floorName,
+    });
+    console.log("resp", resp);
+  } catch (error) {
+    console.log("Could not complete request. View stacktrace for details.");
+
+    console.error(error.response.data);
+  }
+}
 
 module.exports = {
   createNewFloor,
