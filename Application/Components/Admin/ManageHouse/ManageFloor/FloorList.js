8513346e7165dd554e27f670ee33461de2c666f2
Nathan McCloskey <49048346+Rudamentary99@users.noreply.github.com>
2021-02-17 18:44:23 -0600

    created navigator for floor

---
+++ Application/Components/Admin/ManageHouse/ManageFloor/FloorList.js
@@ -0,0 +1,157 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  KeyboardAvoidingView,
+} from "react-native";
+import { FAB, Modal, TextInput, Title, Button, Card } from "react-native-paper";
+
+import { useNavigation } from "@react-navigation/native";
+import { createNewFloor, getFloors } from "./FloorController";
+
+function Floor(props) {
+  const { name, tables, isActive } = props;
+  [viewDetails, setViewDetails] = React.useState(Boolean(props.isEditing));
+
+  const navigation = useNavigation();
+  return (
+    <Card
+      onPress={() => {
+        navigation.navigate("temp");
+      }}
+      style={{
+        width: 250,
+        height: 250,
+        margin: 20,
+      }}
+    >
+      <Card.Title title={name} subtitle="Card Subtitle" />
+    </Card>
+  );
+}
+
+import { floor } from "react-native-reanimated";
+class FloorList extends React.Component {
+  // floors = await getFloors();
+  //const [floors, setFloors] = React.useState(getFloors());
+  constructor(props) {
+    super(props);
+    this.state = {
+      newFloorName: null,
+      floors: null,
+    };
+  }
+  componentDidMount() {
+    getFloors()
+      .then((result) => {
+        this.setState({ floors: result.r });
+      })
+      .catch((err) => {});
+    console.log("floors", this.state.floors);
+  }
+  render() {
+    const { newFloorName, floors } = this.state;
+    const listFloors = () => {
+      return floors.map((floor) => <Floor {...floor} />);
+    };
+    return (
+      <KeyboardAvoidingView behavior="height" style={styles.wrapper}>
+        <ScrollView horizontal style={styles.scrollView}>
+          <View style={styles.floorContainer}>
+            {floors === null ? <Text>loading...</Text> : listFloors()}
+          </View>
+        </ScrollView>
+        <FAB
+          style={styles.fab}
+          icon="plus"
+          onPress={() => {
+            this.setState({ newFloorName: "" });
+          }}
+        />
+        <Modal
+          contentContainerStyle={{ backgroundColor: "white", padding: 20 }}
+          visible={newFloorName !== null}
+          onDismiss={() => {
+            this.setState({ newFloorName: null });
+          }}
+        >
+          <KeyboardAvoidingView behavior="height">
+            <Title>New Floor</Title>
+            <TextInput
+              value={newFloorName}
+              onChangeText={(text) => {
+                this.setState({ newFloorName: text });
+              }}
+              style={{ marginBottom: 30 }}
+              label="Name"
+            ></TextInput>
+            <View style={{ flexDirection: "row-reverse" }}>
+              <Button
+                onPress={() => {
+                  createNewFloor(newFloorName)
+                    .then((result) => {
+                      this.setState({
+                        newFloorName: null,
+                        floors: [...floors, result],
+                      });
+                    })
+                    .catch((err) => {
+                      console.error(err);
+                    });
+                }}
+              >
+                Create
+              </Button>
+              <Button
+                onPress={() => {
+                  this.setState({ newFloorName: null });
+                }}
+              >
+                Cancel
+              </Button>
+            </View>
+          </KeyboardAvoidingView>
+        </Modal>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  wrapper: {
+    position: "absolute",
+    bottom: 0,
+    top: 0,
+    right: 0,
+    left: 0,
+    // backgroundColor: "#e3d7cf",
+  },
+  fab: {
+    position: "absolute",
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
+  scrollView: {
+    position: "relative",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+  },
+  floorContainer: {
+    flex: 1,
+    position: "relative",
+    right: 0,
+    left: 0,
+    flexWrap: "wrap",
+    //backgroundColor: "#f59042",
+  },
+  floor: {
+    width: 250,
+    height: 250,
+    margin: 20,
+  },
+});
+export default FloorList;
--- Application/Components/Admin/ManageHouse/ManageFloor/ManageFloor.js
+++ Application/Components/Admin/ManageHouse/ManageFloor/ManageFloor.js
@@ -9,125 +9,17 @@ import {
 import { FAB, Modal, TextInput, Title, Button, Card } from "react-native-paper";
 import { createNewFloor, getFloors } from "./FloorController";
 import Floor from "./Floor";
-class ManageFloor extends React.Component {
-  // floors = await getFloors();
-  //const [floors, setFloors] = React.useState(getFloors());
-  constructor(props) {
-    super(props);
-    this.state = {
-      newFloorName: null,
-      floors: null,
-    };
-  }
-  componentDidMount() {
-    getFloors()
-      .then((result) => {
-        this.setState({ floors: result.r });
-      })
-      .catch((err) => {});
-    console.log("floors", this.state.floors);
-  }
-  render() {
-    const { newFloorName, floors } = this.state;
-    const listFloors = () => {
-      return floors.map(() => <Floor />);
-    };
-    return (
-      <KeyboardAvoidingView behavior="height" style={styles.wrapper}>
-        <ScrollView horizontal style={styles.scrollView}>
-          <View style={styles.floorContainer}>
-            {floors === null ? <Text>loading...</Text> : listFloors()}
-          </View>
-        </ScrollView>
-        <FAB
-          style={styles.fab}
-          icon="plus"
-          onPress={() => {
-            this.setState({ newFloorName: "" });
-          }}
-        />
-        <Modal
-          contentContainerStyle={{ backgroundColor: "white", padding: 20 }}
-          visible={newFloorName !== null}
-          onDismiss={() => {
-            this.setState({ newFloorName: null });
-          }}
-        >
-          <KeyboardAvoidingView behavior="height">
-            <Title>New Floor</Title>
-            <TextInput
-              value={newFloorName}
-              onChangeText={(text) => {
-                this.setState({ newFloorName: text });
-              }}
-              style={{ marginBottom: 30 }}
-              label="Name"
-            ></TextInput>
-            <View style={{ flexDirection: "row-reverse" }}>
-              <Button
-                onPress={() => {
-                  createNewFloor(newFloorName)
-                    .then((result) => {
-                      this.setState({
-                        newFloorName: null,
-                        floors: [...floors, result],
-                      });
-                    })
-                    .catch((err) => {
-                      console.error(err);
-                    });
-                }}
-              >
-                Create
-              </Button>
-              <Button
-                onPress={() => {
-                  this.setState({ newFloorName: null });
-                }}
-              >
-                Cancel
-              </Button>
-            </View>
-          </KeyboardAvoidingView>
-        </Modal>
-      </KeyboardAvoidingView>
-    );
-  }
-}
-const styles = StyleSheet.create({
-  wrapper: {
-    position: "absolute",
-    bottom: 0,
-    top: 0,
-    right: 0,
-    left: 0,
-    // backgroundColor: "#e3d7cf",
-  },
-  fab: {
-    position: "absolute",
-    margin: 16,
-    right: 0,
-    bottom: 0,
-  },
-  scrollView: {
-    position: "relative",
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-  },
-  floorContainer: {
-    flex: 1,
-    position: "relative",
-    right: 0,
-    left: 0,
-    flexWrap: "wrap",
-    //backgroundColor: "#f59042",
-  },
-  floor: {
-    width: 250,
-    height: 250,
-    margin: 20,
-  },
-});
+import FloorList from "./FloorList";
+
+import { createStackNavigator } from "@react-navigation/stack";
+const Stack = createStackNavigator();
+
+const ManageFloor = (props) => {
+  return (
+    <Stack.Navigator initialRouteName="floor-list" headerMode="none">
+      <Stack.Screen name="floor-list" component={FloorList} />
+    </Stack.Navigator>
+  );
+};
+
 export default ManageFloor;
