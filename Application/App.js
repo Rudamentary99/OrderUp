6237a65774987d7b67ba34d2199006f6df4e6f6f
Nathan McCloskey <49048346+Rudamentary99@users.noreply.github.com>
2021-03-10 18:51:05 -0600

    added drawer for navigation between environments

--- Application/App.js
+++ Application/App.js
@@ -15,13 +15,17 @@ import {
   Portal,
   Provider as PaperProvider,
 } from "react-native-paper";
-import { NavigationContainer, StackActions } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DefaultTheme as navTheme,
+} from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import AdminNav from "./Components/Admin/index";
 import WaiterNav from "./Components/Waiter/index";
 import KitchenNav from "./Components/Kitchen/index";
 const Stack = createStackNavigator();
-
+const Drawer = createDrawerNavigator();
 const tempScreen = ({ navigation }) => {
   const theme = useTheme();
   return (
@@ -32,17 +36,31 @@ const tempScreen = ({ navigation }) => {
 };
 
 export default function App() {
+  const theme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      card: "rgb(255, 255, 255)",
+    },
+  };
+  console.log("navTheme", navTheme);
   // console.log("DefaultTheme", DefaultTheme);
   return (
-    <PaperProvider>
-      <NavigationContainer theme={DefaultTheme}>
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
         <Portal.Host>
-          <Stack.Navigator headerMode="none" initialRouteName="kitchen">
+          <Drawer.Navigator initialRouteName="admin" conten>
+            <Drawer.Screen name="temp" component={tempScreen} />
+            <Drawer.Screen name="admin" component={AdminNav} />
+            <Drawer.Screen name="waiter" component={WaiterNav} />
+            <Drawer.Screen name="kitchen" component={KitchenNav} />
+          </Drawer.Navigator>
+          {/* <Stack.Navigator headerMode="none" initialRouteName="kitchen">
             <Stack.Screen name="temp" component={tempScreen} />
             <Stack.Screen name="admin" component={AdminNav} />
             <Stack.Screen name="waiter" component={WaiterNav} />
             <Stack.Screen name="kitchen" component={KitchenNav} />
-          </Stack.Navigator>
+          </Stack.Navigator> */}
         </Portal.Host>
       </NavigationContainer>
     </PaperProvider>
--- Application/config.js
+++ Application/config.js
@@ -1,5 +1,5 @@
 export default {
   axios: {
-    baseURL: "http://172.25.20.239:3000",
+    baseURL: "http://172.25.17.149:3000",
   },
 };
